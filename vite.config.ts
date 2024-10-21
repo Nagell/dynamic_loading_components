4b8171a0f19f08ea1cf3ffbca4d54658258df17f
Dawid Nitka <dawidnitka@gmail.com>
2024-10-21 21:56:03 +0200

    feat: fixed build setup

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { chunkSplitPlugin } from 'vite-plugin-chunk-split'
 import { getApplicationConfiguration, getProxyConfiguration } from './config/vite'
 
 export default getApplicationConfiguration({
-    cacheDir: '/node_modules/.vite/dynamic_loading_components',
+    cacheDir: './node_modules/.vite/dynamic_loading_components',
 
     server: {
         port: 5005,
@@ -27,7 +27,7 @@ export default getApplicationConfiguration({
     },
 
     build: {
-        outDir: '/dist/dynamic_loading_components',
+        outDir: './dist/dynamic_loading_components',
         // solving vite based storybook issue
         // https://github.com/tailwindlabs/tailwindcss/discussions/3646#discussioncomment-825556
         // sample: https://lobotuerto.com/notes/import-tailwind-config-in-vite
@@ -52,6 +52,8 @@ export default getApplicationConfiguration({
                 // If some custom split is needed filter it out here and return the chunk name
                 // It is recommended to use 'includes' as vue files are transformed in the process,
                 // ex. 'ButtonItem.vue' -> 'ButtonItem.vue_vue_type_script_setup_true_lang-73YVYCtP'
+                if (file.includes('_domain_A.vue')) return 'domain_A'
+                if (file.includes('_domain_B.vue')) return 'domain_B'
                 if (file.includes('node_modules/')) return 'vendor'
                 else return 'index'
             },
